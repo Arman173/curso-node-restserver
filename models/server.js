4c91009b67f9744c7b8c75ca9b8d2d9959c2f7ee
Arman173 <armanhaz173@gmail.com>
2021-12-03 19:28:12 -0500

    .gitignore arreglado, .env, node_modules no monitoreados por git

--- models/server.js
+++ models/server.js
@@ -7,6 +7,7 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+        this.authPath = '/api/auth';
         this.userPath = '/api/user';
 
         // connectar a la base de datos
@@ -36,6 +37,7 @@ class Server {
     }
 
     routes() {
+        this.app.use(this.authPath, require('../routes/auth'));
         this.app.use(this.userPath, require('../routes/user'));
     }
 
